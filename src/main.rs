bef222bc331a3c321aad5113d95d928702167c5f
Gregory Danielson <gregory.danielson3@gmail.com>
2023-02-28 17:41:38 -0600

    chore: roll main blobs into functions

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct Ball {
     color: [f32; 4],
 }
 
-fn gen_ball(balls: &mut Vec<Ball>, pos: [f64; 2]) {
+fn gen_ball(pos: [f64; 2]) -> Ball {
     let mut rng = rand::thread_rng();
     let radius = rng.gen_range(10.0..50.0);
     let x_vel = rng.gen_range(-50.0..50.0);
@@ -24,13 +24,48 @@ fn gen_ball(balls: &mut Vec<Ball>, pos: [f64; 2]) {
         rng.gen_range(0.0..1.0),
         1.0,
     ];
-    balls.push(Ball {
+    Ball {
         x_pos: pos[0],
         y_pos: pos[1],
         x_vel,
         y_vel,
         radius,
         color,
+    }
+}
+
+fn update_balls(balls: &mut Vec<Ball>, dt: f64, window_width: f64, window_height: f64) {
+    for ball in balls {
+        ball.x_pos += ball.x_vel * dt;
+        ball.y_pos += ball.y_vel * dt;
+
+        if ball.x_pos + ball.radius > window_width || ball.x_pos - ball.radius < 0.0 {
+            ball.x_vel = -ball.x_vel;
+        }
+
+        if ball.y_pos + ball.radius > window_height || ball.y_pos - ball.radius < 0.0 {
+            ball.y_vel = -ball.y_vel;
+        }
+    }
+}
+
+fn render_balls(balls: &Vec<Ball>, window: &mut PistonWindow, event: &Event) {
+    window.draw_2d(event, |context, graphics, _device| {
+        clear([0.0, 0.0, 0.0, 1.0], graphics);
+
+        for ball in balls {
+            ellipse(
+                ball.color,
+                [
+                    ball.x_pos - ball.radius,
+                    ball.y_pos - ball.radius,
+                    ball.radius * 2.0,
+                    ball.radius * 2.0,
+                ],
+                context.transform,
+                graphics,
+            );
+        }
     });
 }
 
@@ -44,50 +79,25 @@ fn main() {
 
     while let Some(event) = window.next() {
         if let Some(_) = event.render_args() {
-            window.draw_2d(&event, |context, graphics, _device| {
-                clear([0.0, 0.0, 1.0, 1.0], graphics);
-
-                for ball in &balls {
-                    ellipse(
-                        ball.color,
-                        [
-                            ball.x_pos - ball.radius,
-                            ball.y_pos - ball.radius,
-                            ball.radius * 2.0,
-                            ball.radius * 2.0,
-                        ],
-                        context.transform,
-                        graphics,
-                    );
-                }
-            });
+            render_balls(&balls, &mut window, &event);
         }
 
         if let Some(pos) = event.mouse_cursor_args() {
             last_mouse_pos = pos;
-        } else {
-            println!("No mouse position data");
         }
 
         if let Some(Button::Mouse(MouseButton::Left)) = event.press_args() {
-            gen_ball(&mut balls, last_mouse_pos)
+            let ball = gen_ball(last_mouse_pos);
+            balls.push(ball);
         }
 
         if let Some(update) = event.update_args() {
-            for ball in &mut balls {
-                ball.x_pos += ball.x_vel * update.dt;
-                ball.y_pos += ball.y_vel * update.dt;
-
-                if ball.x_pos + ball.radius > window.size().width || ball.x_pos - ball.radius < 0.0
-                {
-                    ball.x_vel = -ball.x_vel;
-                }
-
-                if ball.y_pos + ball.radius > window.size().height || ball.y_pos - ball.radius < 0.0
-                {
-                    ball.y_vel = -ball.y_vel;
-                }
-            }
+            update_balls(
+                &mut balls,
+                update.dt,
+                window.size().width,
+                window.size().height,
+            );
         }
     }
 }
